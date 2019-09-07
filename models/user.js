25bd12f1e6b7940dc31c2104727f29e611410ff6
AM98 <andymendez100@gmail.com>
2019-09-07 14:47:55 -0700

    Working on passport

---
+++ models/user.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+// User Schema
+const UserSchema = mongoose.Schema({
+  username: {
+    type: String,
+    index: true,
+  },
+  password: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  name: {
+    type: String,
+  },
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports.User = User;
+
+module.exports.createUser = function(newUser, callback) {
+  bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash(newUser.password, salt, function(err, hash) {
+      newUser.password = hash;
+      newUser.save(callback);
+    });
+  });
+};
+
+module.exports.getUserByUsername = function(username, callback) {
+  const query = { username };
+  User.findOne(query, callback);
+};
+
+module.exports.getUserById = function(id, callback) {
+  User.findById(id, callback);
+};
+
+module.exports.comparePassword = function(candidatePassword, hash, callback) {
+  bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
+    if (err) throw err;
+    callback(null, isMatch);
+  });
+};
