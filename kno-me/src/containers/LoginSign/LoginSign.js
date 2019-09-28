dae0481abc136cfbeca2e084741cee37355043b2
bugzweiser <bugzweiser@gmail.com>
2019-09-28 13:00:10 -0700

    corrections, need to float left to right

--- kno-me/src/containers/LoginSign/LoginSign.js
+++ kno-me/src/containers/LoginSign/LoginSign.js
@@ -63,151 +63,151 @@ export default function SignUp() {
   const classes = useStyles();
 
   return (
+
     <Container component="main" maxWidth="xl">
 
-      <Grid item item xs={12} sm={6}>
-
+    <Container component="main" maxWidth="xs">
+      
+      <CssBaseline />
       <div className={classes.paper}>
-            <CssBaseline />
-                    <div className={classes.paper}>
-                      <Avatar className={classes.avatar}>
-                        <LockOutlinedIcon />
-                      </Avatar>
-                      <Typography component="h1" variant="h5">
-                        Sign in
-                      </Typography>
-                      <form className={classes.form} noValidate>
-                        <TextField
-                          variant="outlined"
-                          margin="normal"
-                          required
-                          fullWidth
-                          id="email"
-                          label="Email Address"
-                          name="email"
-                          autoComplete="email"
-                          autoFocus
-                        />
-                        <TextField
-                          variant="outlined"
-                          margin="normal"
-                          required
-                          fullWidth
-                          name="password"
-                          label="Password"
-                          type="password"
-                          id="password"
-                          autoComplete="current-password"
-                        />
-                        <FormControlLabel
-                          control={<Checkbox value="remember" color="primary" />}
-                          label="Remember me"
-                        />
-                        <Button
-                          type="submit"
-                          fullWidth
-                          variant="contained"
-                          color="primary"
-                          className={classes.submit}
-                        >
-                          Sign In
-                        </Button>
-                        <Grid container>
-                          <Grid item xs>
-                            <Link href="#" variant="body2">
-                              Forgot password?
-                            </Link>
-                          </Grid>
-                        </Grid>
-                      </form>
-                    </div>
-              </div>     
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
             </Grid>
+          </Grid>
+        </form>
+      </div>
 
+      </Container>
 
-            <Grid item item xs={12} sm={6}>
-            <div className={classes.paper}>
-                    <CssBaseline />
-                    <div className={classes.paper}>
-                      <Avatar className={classes.avatar}>
-                        <LockOutlinedIcon />
-                      </Avatar>
-                      <Typography component="h1" variant="h5">
-                        Sign up
-                      </Typography>
-                      <form className={classes.form} noValidate>
-                        <Grid container spacing={2}>
-                          <Grid item xs={12} sm={6}>
-                            <TextField
-                              autoComplete="fname"
-                              name="firstName"
-                              variant="outlined"
-                              required
-                              fullWidth
-                              id="firstName"
-                              label="First Name"
-                              autoFocus
-                            />
-                          </Grid>
-                          <Grid item xs={12} sm={6}>
-                            <TextField
-                              variant="outlined"
-                              required
-                              fullWidth
-                              id="lastName"
-                              label="Last Name"
-                              name="lastName"
-                              autoComplete="lname"
-                            />
-                          </Grid>
-                          <Grid item xs={12}>
-                            <TextField
-                              variant="outlined"
-                              required
-                              fullWidth
-                              id="email"
-                              label="Email Address"
-                              name="email"
-                              autoComplete="email"
-                            />
-                          </Grid>
-                          <Grid item xs={12}>
-                            <TextField
-                              variant="outlined"
-                              required
-                              fullWidth
-                              name="password"
-                              label="Password"
-                              type="password"
-                              id="password"
-                              autoComplete="current-password"
-                            />
-                          </Grid>
-                          <Grid item xs={12}>
-                            <FormControlLabel
-                              control={<Checkbox value="allowExtraEmails" color="primary" />}
-                              label="I want to receive inspiration, marketing promotions and updates via email."
-                            />
-                          </Grid>
-                        </Grid>
-                        <Button
-                          type="submit"
-                          fullWidth
-                          variant="contained"
-                          color="primary"
-                          className={classes.submit}
-                        >
-                          Sign Up
-                        </Button>
-                      </form>
-                    </div>
-        
-            <Box mt={5}>
-              <Copyright />
-            </Box>
-                  </div>
+    <Container component="main" maxWidth="xs">
+
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <form className={classes.form} noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete="fname"
+                name="firstName"
+                variant="outlined"
+                required
+                fullWidth
+                id="firstName"
+                label="First Name"
+                autoFocus
+              />
             </Grid>
-     
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="lastName"
+                label="Last Name"
+                name="lastName"
+                autoComplete="lname"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <FormControlLabel
+                control={<Checkbox value="allowExtraEmails" color="primary" />}
+                label="I want to receive inspiration, marketing promotions and updates via email."
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign Up
+          </Button>
+          <Grid container justify="flex-end">
+          </Grid>
+        </form>
+      </div>  
+      </Container>
+
+      <Box mt={5}>
+        <Copyright />
+      </Box>
+    
     </Container>
   );
 }
