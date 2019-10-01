2e1bcdc926e98d3b9396ecb5534f8b40b5f1db1a
Sayne Rojas <saynegrojas@yahoo.com>
2019-10-01 04:16:11 -0700

    fixed signin/signup

--- kno-me/src/components/loginSign/LoginSign.js
+++ kno-me/src/components/loginSign/LoginSign.js
@@ -11,37 +11,37 @@ const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
     // margin: '15% 15%', smallest size
-    margin: '20% 10%',
-    position: '100% 100%',
+    margin: '10% 20%',
     boxShadow: '0 5px 10px 0 ',
   },
   labelText: {
     color: '#3f51b5',
+    textAlign: 'left'
   },
   label: {
-    marginLeft: '25%'
+    marginLeft: '50%'
   },
   paper: {
-    marginTop: theme.spacing(1),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    height: '100%'
   },
   avatar: {
-    marginLeft: '25%',
+    marginLeft: '50%',
     backgroundColor: theme.palette.secondary.main,
   },
   form: {
     width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(0),
-    paddingLeft: '15px'
+    marginTop: theme.spacing(2),
+    justifyContent: 'left',
+    margin: '10%'
   },
   submit: {
     margin: theme.spacing(2, 0, 2, 2),
     paddingLeft: '15px'
   },
 }));
-
 export default function LoginSign(props) {
   const classes = useStyles();
 
@@ -107,196 +107,137 @@ export default function LoginSign(props) {
       });
   };
 
+
   return (
     <Paper className={classes.root}>
+
       {/* Sign In */}
-      <Grid
-        container
-        direction="row"
-        justify="center"
-        alignItems="center"
-      >
-        <Grid>
-          <Grid>
-            <Avatar className={classes.avatar}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography className={classes.label}>
-              Sign In
-          </Typography>
-          </Grid>
-          <form onSubmit={signIn}>
-            <Grid item sm={12}>
-              <Grid
-                container
-                justify="center"
-                alignItems="center"
-              >
+      < Grid container spacing={0} >
+
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>
+            <Grid container justify="flex-start">
+              <Avatar className={classes.avatar}>
+                <LockOutlinedIcon />
+              </Avatar>
+              <Typography className={classes.label}>
+                Sign In
+              </Typography>
+              <form onSubmit={signIn} className={classes.form}>
                 <Typography className={classes.labelText}>
                   Username
                </Typography>
                 <TextField
                   variant="outlined"
-                  margin="normal"
+                  fullWidth
                   id="loginUsername"
                   type="text"
                   label="enter your username"
                 />
-              </Grid>
-            </Grid>
-            <Grid item sm={12}>
-              <Grid
-                container
-                justify="center"
-                alignItems="center"
-              >
                 <Typography className={classes.labelText}>
                   Password
-              </Typography>
+                </Typography>
                 <TextField
                   variant="outlined"
-                  margin="normal"
+                  fullWidth
                   id="loginPassword"
                   type="password"
                   label="enter your password"
                 />
-              </Grid>
+                <Button
+                  color="primary"
+                  variant="contained"
+                  type="submit"
+                  id="loginButton"
+                  className={classes.submit}
+                >
+                  Login
+                </Button>
+              </form>
             </Grid>
-            <Button
-              // className={classes.submit}
-              color="primary"
-              variant="contained"
-              type="submit"
-              id="loginButton"
-              className={classes.submit}
-            >
-              Login
-          </Button>
-          </form>
+          </Paper>
         </Grid>
-
-
 
         {/* Sign Up */}
-        <Grid>
-          <Grid>
-            <Avatar className={classes.avatar}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography className={classes.label}>
-              Sign Up
-          </Typography>
-          </Grid>
-          <Grid>
-            <form onSubmit={signUp}>
-              <Grid item sm={12}>
-                <Grid
-                  container
-                  justify="center"
-                  alignItems="center"
-                >
-                  <Typography className={classes.labelText}>
-                    Name
-                  </Typography>
-                  <TextField
-                    variant="outlined"
-                    margin="normal"
-                    id="signUpName"
-                    type="text"
-                    label="enter your name"
-                    name="name"
-                  />
-                </Grid>
-              </Grid>
-              <Grid item sm={12}>
-                <Grid
-                  container
-                  justify="center"
-                  alignItems="center"
-                >
-                  <Typography className={classes.labelText}>
-                    Email
-              </Typography>
-                  <TextField
-                    variant="outlined"
-                    margin="normal"
-                    id="signUpEmail"
-                    type="email"
-                    label="enter your email"
-                    name="email"
-                  />
-                </Grid>
-              </Grid>
-              <Grid item sm={12}>
-                <Grid
-                  container
-                  justify="center"
-                  alignItems="center"
-                >
-                  <Typography className={classes.labelText}>
-                    Username
-              </Typography>
-                  <TextField
-                    variant="outlined"
-                    margin="normal"
-                    id="signUpUsername"
-                    type="text"
-                    label="enter your username"
-                    name="username"
-                  />
-                </Grid>
-              </Grid>
-              <Grid item sm={12}>
-                <Grid
-                  container
-                  justify="center"
-                  alignItems="center"
-                >
-                  <Typography className={classes.labelText}>
-                    Password
-              </Typography>
-                  <TextField
-                    variant="outlined"
-                    margin="normal"
-                    id="signUpPassword"
-                    type="password"
-                    label="enter a password"
-                    name="password"
-                  />
-                </Grid>
-              </Grid>
-              <Grid item sm={12}>
-                <Grid
-                  container
-                  justify="center"
-                  alignItems="center"
-                >
-                  <Typography className={classes.labelText}>
-                    Confirm Password
-              </Typography>
-                  <TextField
-                    variant="outlined"
-                    margin="normal"
-                    id="signUpPassword2"
-                    type="password"
-                    label="confirm your password"
-                    name="password2"
-                  />
-                </Grid>
-              </Grid>
-              <Button
-                variant="contained"
-                color="primary"
-                type="submit"
-                id="signUpButton"
-                className={classes.submit}
-              >
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>
+            <Grid container justify="flex-start">
+              <Avatar className={classes.avatar}>
+                <LockOutlinedIcon />
+              </Avatar>
+              <Typography className={classes.label}>
                 Sign Up
-            </Button>
-            </form>
-          </Grid>
+                </Typography>
+              <form onSubmit={signUp} className={classes.form}>
+                <Typography className={classes.labelText}>
+                  Name
+                  </Typography>
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  id="signUpName"
+                  type="text"
+                  label="enter your name"
+                  name="name"
+                />
+                <Typography className={classes.labelText}>
+                  Email
+                      </Typography>
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  id="signUpEmail"
+                  type="email"
+                  label="enter your email"
+                  name="email"
+                />
+                <Typography className={classes.labelText}>
+                  Username
+                      </Typography>
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  id="signUpUsername"
+                  type="text"
+                  label="enter your username"
+                  name="username"
+                />
+                <Typography className={classes.labelText}>
+                  Password
+                      </Typography>
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  id="signUpPassword"
+                  type="password"
+                  label="enter a password"
+                  name="password"
+                />
+                <Typography className={classes.labelText}>
+                  Confirm Password
+                      </Typography>
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  id="signUpPassword2"
+                  type="password"
+                  label="confirm your password"
+                  name="password2"
+                />
+                <Button
+                  variant="contained"
+                  color="primary"
+                  type="submit"
+                  id="signUpButton"
+                  className={classes.submit}
+                >
+                  Sign Up
+                   </Button>
+              </form>
+            </Grid>
+          </Paper>
         </Grid>
-      </Grid>
+      </Grid >
     </Paper>
   );
 }
